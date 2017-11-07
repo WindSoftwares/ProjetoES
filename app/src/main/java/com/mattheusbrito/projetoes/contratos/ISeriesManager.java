dedf181574b51d14812ccda6a6c46788346961da
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2017-11-07 01:10:12 -0300

    contract clauses have been addded in IUser

--- app/src/main/java/com/mattheusbrito/projetoes/contratos/ISeriesManager.java
+++ app/src/main/java/com/mattheusbrito/projetoes/contratos/ISeriesManager.java
@@ -1,8 +1,19 @@
 package com.mattheusbrito.projetoes.contratos;
 
+import com.mattheusbrito.projetoes.model.series.Serie;
+
+import java.util.Set;
+
 /**
  * Created by gersonsales on 07/11/17.
  */
 
 public interface ISeriesManager {
+
+    public Serie getSerie(String seriesName);
+    public Set<Serie> getSeries();
+    public Set<Serie> getStarredSeries();
+    public void starASerie(String seriesName);
+    public void unstarASerie(String seriesName);
+    public String getSerieInformation(String seriesName);
 }
---
+++ app/src/main/java/com/mattheusbrito/projetoes/model/series/Serie.java
@@ -0,0 +1,8 @@
+package com.mattheusbrito.projetoes.model.series;
+
+/**
+ * Created by gersonsales on 07/11/17.
+ */
+
+public class Serie {
+}
