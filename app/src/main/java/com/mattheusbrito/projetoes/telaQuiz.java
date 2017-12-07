89ee1521953c8dae992a55a05c40855e94b60d78
joaolcaas <joao.felipe@ccc.ufcg.edu.br>
2017-12-07 16:52:02 -0300

    refatoramento da activity

--- app/src/main/java/com/mattheusbrito/projetoes/telaQuiz.java
+++ app/src/main/java/com/mattheusbrito/projetoes/telaQuiz.java
@@ -16,141 +16,91 @@ import java.util.Set;
 public class telaQuiz extends AppCompatActivity {
 
 
-    private Button botao1Resposta,botao2Resposta,botao3Resposta,botao4Resposta;
+    private Button botao1Resposta, botao2Resposta, botao3Resposta, botao4Resposta;
     private TextView perguntaTxt;
     //variavel para teste da tela
     private int cont = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_quiz);
 
-        botao1Resposta = (Button)findViewById(R.id.resposta1Button);
-        botao2Resposta = (Button)findViewById(R.id.resposta2Button);
-        botao3Resposta = (Button)findViewById(R.id.resposta3Button);
-        botao4Resposta = (Button)findViewById(R.id.resposta4Button);
-
-        perguntaTxt = (TextView)findViewById(R.id.perguntaTxt);
+        carregaBotoes();
+        //primeira pergunta do quiz
         perguntaTxt.setText("Primeira pergunta");
+
 
         botao1Resposta.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-                perguntaTxt.setText("Pergunta do botao1 ");
+            public void onClick(View view) {
                 cont++;
                 //onde vamos verificar se a resposta do usuário está correta
-                if(botao1Resposta.getText().equals("botao")){
-                    botao1Resposta.setBackgroundColor(Color.parseColor("#33cc33"));
-                    //enable para não poder clicar em outros botoes
-                    setClickButtons(false);
-                }else{
-                    botao1Resposta.setBackgroundColor(Color.parseColor("#cc0000"));
-                    //enable para não poder clicar em outros botoes
-                    setClickButtons(false);
-                }
-
-                //mudo a cor do background e seto tudo depois de 2 s
+                verificaResposta("botao",botao1Resposta);
+                //mudo a cor do background e seto tudo depois de 1 s
                 Handler handler = new Handler();
                 handler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        mudaBotoes();
-                        perguntaTxt.setText("pergunta botao1");
-                        botao1Resposta.setBackgroundColor(Color.parseColor("#f5f5f0"));
-                        setClickButtons(true);
-                        if(acabaQuiz(cont)){
-                            /*
-                            no nosso caso, teriamos o numero de jogadas do usuario e mudariamos para a tela da pontuacao
-                             */
-                            finish();
-                        }
+                       /*
+                       quando tivermas a questao, iremos passar a pergunta daquela questao
+                       inPlay(questao.getDescription())
+                        */
+                        inPlay("pergunta botao1",botao1Resposta);
+
                     }
-                },2000);
+                }, 1000);
 
             }
         });
         botao2Resposta.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-
-                if(botao2Resposta.getText().equals("botao")){
-                    botao2Resposta.setBackgroundColor(Color.parseColor("#33cc33"));
-                    setClickButtons(false);
-                }else{
-                    botao2Resposta.setBackgroundColor(Color.parseColor("#cc0000"));
-                    setClickButtons(false);
-                }
+            public void onClick(View view) {
+                cont++;
+              verificaResposta("botao",botao2Resposta);
 
                 Handler handler = new Handler();
                 handler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        mudaBotoes();
-                        perguntaTxt.setText("Pergunta do botao2");
-                        botao2Resposta.setBackgroundColor(Color.parseColor("#f5f5f0"));
-                        setClickButtons(true);
-                        if(acabaQuiz(cont)){
-                            finish();
-                        }
+
+                        inPlay("pergunta botao2",botao2Resposta);
+
                     }
-                },2000);
+                }, 1000);
             }
         });
 
         botao3Resposta.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-                perguntaTxt.setText("Pergunta do botao3");
-
-                if(botao3Resposta.getText().equals("botao")){
-                    botao3Resposta.setBackgroundColor(Color.parseColor("#33cc33"));
-                    setClickButtons(false);
-                }else{
-                    botao3Resposta.setBackgroundColor(Color.parseColor("#cc0000"));
-                    setClickButtons(false);
-                }
+            public void onClick(View view) {
+                cont++;
+                verificaResposta("botao",botao3Resposta);
                 Handler handler = new Handler();
                 handler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        mudaBotoes();
-                        perguntaTxt.setText("pergunta botao3");
-                        botao3Resposta.setBackgroundColor(Color.parseColor("#f5f5f0"));
-                        setClickButtons(true);
-                        if(acabaQuiz(cont)){
-                            finish();
-                        }
+
+                        inPlay("pergunta botao3",botao3Resposta);
                     }
-                },2000);
+                }, 1000);
 
             }
         });
 
         botao4Resposta.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-                perguntaTxt.setText("Pergunta do botao4 ");
-
-                if(botao4Resposta.getText().equals("botao")){
-                    botao4Resposta.setBackgroundColor(Color.parseColor("#33cc33"));
-                    setClickButtons(false);
-                }else{
-                    botao4Resposta.setBackgroundColor(Color.parseColor("#cc0000"));
-                    setClickButtons(false);
-                }
+            public void onClick(View view) {
+                cont++;
+               verificaResposta("botao",botao4Resposta);
                 Handler handler = new Handler();
                 handler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        mudaBotoes();
-                        perguntaTxt.setText("pergunta botao4");
-                        botao4Resposta.setBackgroundColor(Color.parseColor("#f5f5f0"));
-                        setClickButtons(true);
-                        if(acabaQuiz(cont)){
-                            finish();
-                        }
+                        inPlay("pergunta botao4",botao4Resposta);
+
                     }
-                },2000);
+                }, 1000);
 
             }
         });
@@ -159,7 +109,7 @@ public class telaQuiz extends AppCompatActivity {
     /**
      * metodo onde vamos mudar todas as respostas dos botoes, para que o usuario possa responder
      */
-    public void mudaBotoes(){
+    public void mudaBotoes() {
         botao1Resposta.setText("outra resposta ");
         botao2Resposta.setText("outra resposta ");
         botao3Resposta.setText("outra resposta ");
@@ -168,9 +118,10 @@ public class telaQuiz extends AppCompatActivity {
 
     /**
      * metodo em que vamos setar se o botao e "clicável" ou não
+     *
      * @param enableOrNot
      */
-    public void setClickButtons(boolean enableOrNot){
+    public void setClickButtons(boolean enableOrNot) {
         botao1Resposta.setClickable(enableOrNot);
         botao2Resposta.setClickable(enableOrNot);
         botao3Resposta.setClickable(enableOrNot);
@@ -179,13 +130,62 @@ public class telaQuiz extends AppCompatActivity {
 
     /**
      * metodo que vamos usar para saber quando o quiz acabada(alternativa)
-     * @return boolean
+     *
      * @param jogadas
+     * @return boolean
      */
-    public boolean acabaQuiz(int jogadas){
-        if(jogadas == 10){
+    public boolean acabaQuiz(int jogadas) {
+        if (jogadas == 10) {
             return true;
         }
         return false;
+    }
+
+    /**
+     * metodo que vai fazer a tela aparecer jogavel depois de uma jogada
+     * @param description
+     */
+    public void inPlay(String description,Button button) {
+        if (acabaQuiz(cont)) {
+            /*
+             aqui ficaria a chamada da proxima tela, depois de responder todas as perguntas
+            */
+            finish();
+        } else {
+            mudaBotoes();
+            /*seta a questao
+             */
+            perguntaTxt.setText(description);
+            button.setBackgroundColor(Color.parseColor("#47476b"));
+            setClickButtons(true);
+        }
+
+    }
+
+    /**
+     * metodo que carrega os botoes que serao usados na activity
+     */
+    public void carregaBotoes(){
+        botao1Resposta = (Button) findViewById(R.id.resposta1Button);
+        botao2Resposta = (Button) findViewById(R.id.resposta2Button);
+        botao3Resposta = (Button) findViewById(R.id.resposta3Button);
+        botao4Resposta = (Button) findViewById(R.id.resposta4Button);
+        perguntaTxt = (TextView) findViewById(R.id.perguntaTxt);
+    }
+
+    /**
+     * metodo que vai verificar a resposta de acordo com o botao
+     * @param respostaCerta
+     */
+    public void verificaResposta(String respostaCerta,Button button){
+        if (button.getText().equals(respostaCerta)) {
+            button.setBackgroundColor(Color.parseColor("#33cc33"));
+            //enable para não poder clicar em outros botoes
+            setClickButtons(false);
+        } else {
+            button.setBackgroundColor(Color.parseColor("#cc0000"));
+            //enable para não poder clicar em outros botoes
+            setClickButtons(false);
+        }
     }
 }
